5bd92cbe8b5fc5bc2538cf4e01ffd7fc33c9878d
Jean Perrault <perrault.jean66@gmail.com>
2019-02-19 14:08:22 +0100

    -

---
+++ private/app/src/app.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once "../private/app/config/config.php";
+require_once "../private/app/src/environnement.php";
+require_once "../private/app/src/err_reporting.php";
+require_once "../private/app/src/db_connect.php";
+require_once "../private/app/src/routing.php";
+require_once "../private/app/src/compile.php";
+
+
+// require_once "./app/config/config.php";
+// require_once "./src/environnement.php";
+// require_once "./src/err_reporting.php";
+// require_once "./src/db_connect.php";
+// require_once "./src/routing.php";
+// require_once "./src/compile.php";
