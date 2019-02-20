109e579e9a20ddf6479bac9481a57f696ee45db0
Jean Perrault <perrault.jean66@gmail.com>
2019-02-20 13:38:59 +0100

    -

--- private/app/utils/randstr.php
+++ private/app/utils/randstr.php
@@ -4,9 +4,24 @@
  *
  * @return void
  */
-if (!function_exists('randstr')){
-    function randstr($length = 10){
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+if (!function_exists('randstr')) {
+    function randstr($length=10, $upper=false, $number=false) 
+    {
+        $characters = 'abcdefghijklmnopqrstuvwxyz';
+        $characters_upper = strtoupper($characters);
+        $characters_number = '0123456789';
+
+        if ($upper) {
+            $characters.= $characters_upper;
+            var_dump($characters);
+        }
+
+        if ($number) {
+            $characters .= $characters_number;
+            var_dump($characters);
+        }
+                
         $charactersLength = strlen($characters);
         $randomString = '';
         for ($i = 0; $i < $length; $i++) {
@@ -15,3 +30,15 @@ if (!function_exists('randstr')){
         return $randomString;
     }
 }
+
+// if (!function_exists('randstr')){
+//     function randstr($length = 10){
+//         $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+//         $charactersLength = strlen($characters);
+//         $randomString = '';
+//         for ($i = 0; $i < $length; $i++) {
+//             $randomString .= $characters[rand(0, $charactersLength - 1)];
+//         }
+//         return $randomString;
+//     }
+// }
