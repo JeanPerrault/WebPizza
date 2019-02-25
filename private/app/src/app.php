5f9760fcc1bae660881399eb183cc2e951acec5f
Jean Perrault <perrault.jean66@gmail.com>
2019-02-25 17:22:27 +0100

    -

--- private/app/src/app.php
+++ private/app/src/app.php
@@ -1,33 +1,46 @@
 <?php
-
+/**
+ * Fichier d'exécution de l'application
+ */
 
 /**
- * 1. integration de la configuration
+ * 1. Démarrage de la session
+ * --
+ * La session va permettre de suivre le visiteur pendant la durée de sa navigation
+ */
+session_start();
+
+/**
+ * 2. Intégration de la configuration
  */
 require_once "../private/app/config/config.php";
+
 /**
- * 2. definition de l'environnement
+ * 3. Définition de l'environnement
  */
 require_once "../private/app/src/environnement.php";
+
 /**
- * 3. comportement des erreurs
+ * 4. Comportement des erreurs
  */
 require_once "../private/app/src/err_reporting.php";
+
 /**
- * 4. connection aux bases de donnees
+ * 5. Connections aux base de données
  */
 require_once "../private/app/src/db_connect.php";
+
 /**
- * 5. routage de l application
+ * 6. Routage de l'application
  */
 require_once "../private/app/src/routing.php";
+
 /**
- * 6. inclusion des fonctions 'Utils"
+ * 7. Inclusion des fonctions "Utils"
  */
 require_once "../private/app/src/load_utils.php";
+
 /**
- * 7. compilation de la page
+ * 8. Compilation de la page
  */
 require_once "../private/app/src/compile.php";
-
-
