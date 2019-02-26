64e423e37d2d295e9967f8ee092278d4c6a6b4c1
Jean Perrault <perrault.jean66@gmail.com>
2019-02-26 11:10:15 +0100

    -

--- private/src/controllers/contact.php
+++ private/src/controllers/contact.php
@@ -1,89 +1,90 @@
 <?php
 /**
- * fichier qui gere la page de contact
+ * Fichier qui gère la page de contact
  */
 
- /**
-  * index
-  */
-
-  function contact_index(){
-
+/**
+ * index
+ */
+function contact_index() 
+{
     global $re, $db;
 
-    if($_SERVER['REQUEST_METHOD'] === "POST"){
-
-        // declaration de la variable d'envoi du formaulaire
+    if ($_SERVER['REQUEST_METHOD'] === "POST")
+    {
         $send = true;
 
-        // recuperation des donnees 
+        // Récupération des données
         $firstname = isset($_POST['firstname']) ? $_POST['firstname'] : null;
         $lastname = isset($_POST['lastname']) ? $_POST['lastname'] : null;
         $email = isset($_POST['email']) ? $_POST['email'] : null;
         $message = isset($_POST['message']) ? $_POST['message'] : null;
+    
+        // -- Contrôle des champs
 
-        // -- controle des champs
-
-        // controle du champ prenom
-        if(!preg_match($re['firstname'], $firstname)){
-          $send = false;
-          //echo "Erreur du champ Firstname";
+        // Contrôle du champ Prénom
+        if (!preg_match($re['firstname'], $firstname)) {
+            $send = false;
+            echo "Erreur du champ Firstname";
         }
-        // controle du champ nom
-        if(!preg_match($re['lastname'], $lastname)){
-          $send = false;
-          //echo "Erreur du champ Lastname";
+        
+        // Contrôle du champ nom
+        if (!preg_match($re['lastname'], $lastname)) {
+            $send = false;
+            echo "Erreur du champ Lastname";
         }
-        // controle du champ email
-        if(!preg_match($re['email'], $email)){
-          $send = false;
-          //echo "Erreur du champ Email";
-        }
-        // controle du champ message
-        if(strlen($message) < 10){
-          $send = false;
-          //echo "Erreur du champ Message";
+        
+        // Contrôle du champ email
+        if (!preg_match($re['email'], $email)) {
+            $send = false;
+            echo "Erreur du champ Email";
         }
 
-        if ($send){
-          // TODO: Enregistrement du message dans la BDD avec PDO
-            $query = $db['main']-> prepare("INSERT INTO messages (`firstname`, `lastname`, `email`,`message`, `sending_timestamp`) 
-                                                          VALUES (:firstname, :lastname, :email, :message, :sendingtimestamp)");
-            // attention de faire correspondre le type de param avec le type des champs                                                           
-            $query-> bindValue(':firstname', $firstname, PDO::PARAM_STR);
-            $query-> bindValue(':lastname', $lastname, PDO::PARAM_STR);
-            $query-> bindValue(':email', $email, PDO::PARAM_STR);
-            $query-> bindValue(':message', $message, PDO::PARAM_STR);
-            $query-> bindValue(':sendingtimestamp', time(), PDO::PARAM_INT);
-            $results = $query->execute(); // $results = true si ok sinon false
-            if($results){
-                            
-            }else{
+        // Contrôle du champ message
+        if (strlen($message) < 10) {
+            $send = false;
+            echo "Erreur du champ message";
+        }
 
+        if ($send) {
+            // Enregistrement du message dans la BDD avec PDO
+
+            $query = $db['main']->prepare("INSERT INTO messages (`firstname`, `lastname`, `email`, `message`, `sending_timestamp`) 
+                                                         VALUES (:firstname , :lastname , :email , :message, :sendingTimestamp )");
+
+            $query->bindValue(':firstname', $firstname, PDO::PARAM_STR);
+            $query->bindValue(':lastname', $lastname, PDO::PARAM_STR);
+            $query->bindValue(':email', $email, PDO::PARAM_STR);
+            $query->bindValue(':message', $message, PDO::PARAM_STR);
+            $query->bindValue(':sendingTimestamp', time(), PDO::PARAM_INT);
+
+            $results = $query->execute();
+
+            // TODO: Définition d'un message de "callback / flashbag" (success ou error)
+            if ($results) {
+                setFlashbag("success", "Merci $firstname, Votre message à était envoyé.");
+            }
+            else {
+                setFlashbag("danger", "Votre message n'à pas était envoyé.");
             }
 
-          // TODO: Definition d'un message de "callback / flashbag" (success ou error)
-          // TODO: redirection de l utilisateur vers la page precedente
-          
-          //echo "On enregistre le message dans la BDD";
-        }else{
-          // TODO: Definition d'un message de "callback" (error)
-          // TODO: redirection de l utilisateur vers la page precedente
+            // Redirection de l'utilisateur vers la page précedente 
+            header("location: ".$_SERVER['HTTP_REFERER']);
 
-          //echo "Erreur sur le formulaire";
         }
+        // else {
+            // TODO: Définition d'un message de "callback" (error)
+            // TODO: Redirection de l'utilisateur vers la page précedente 
+        //     echo "Erreur sur le form";
+        // }
 
-        // dump($_POST);
-        // dump($firstname);
-        // dump($lastname);
-        // dump($email);
-        // dump($message);
-
-    }else {
-          
-        // TODO: suppression du else + redirection de l utilisateur
-        // TODO: Definition d'un message de "callback" (error)
-        // TODO: redirection de l utilisateur vers la page precedente
-        echo "Le formulaire ne peut etre traité qu avec la methode POST";
     }
+
+    else {
+        // TODO: Suppression du else + redirection de l'utilisateur
+        // TODO: Définition d'un message de "callback" (error)
+        // TODO: Redirection de l'utilisateur vers la page précedente 
+        echo "Le formulaire ne peut être traité qu'avec la méthode POST";
+    }
+
 }
